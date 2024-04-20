a49e3326230e7d46dc848e95366cfaa7edfbc188
Louis Pilfold <louis@lpil.uk>
2024-04-20 11:28:15 +0100

    Tweak

--- bin/all-emails.sh
+++ bin/all-emails.sh
@@ -10,21 +10,9 @@ sqlite3 "$DB_FILE" <<EOF
 .bail on
 .mode column
 
-with money as (
-  select reference, sum(amount) as amount
-  from payments
-  group by reference
-)
-
 select distinct
   users.email as email
-from users
-inner join
-  applications on applications.user_id = users.id
-left join
-  payments on payments.reference = applications.payment_reference
-where
-  payments.id is not null;
+from users;
 
 .quit
 EOF
